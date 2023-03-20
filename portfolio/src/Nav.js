d6b8613ba2b048725f85b960b3a3d78c08776634
Your Name <you@example.com>
2023-03-20 13:28:28 +0100

    test

--- portfolio/src/Nav.js
+++ portfolio/src/Nav.js
@@ -32,22 +32,8 @@ export default function Nav() {
      */
 
 
-  return (
-    <nav>
-        
+  return 
+
   
-    
-    
-    <input type="checkbox" id="active"></input>
-    <label htmlFor="active" className='menu-btn'><FontAwesomeIcon icon={faBars} size="lg" /></label> 
-    <div className='wrapper'>
-        <ul className='menu'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>Timeline</a></li>
-            <li><a href='#'>Projects</a></li>
-            <li><a href='#'>Contact</a></li>
-        </ul>
-    </div>
-    </nav>
-  )
+
 }
